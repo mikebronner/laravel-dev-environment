9e5660e72400e3b21ddd92e356e4b501f7e9dc9f
Mike Bronner <mike@genealabs.com>
2015-08-29 08:35:58 -0700

    Fix sed command (remove r suffix)

--- ZendServer/Homestead/post-install-permissions.sh
+++ ZendServer/Homestead/post-install-permissions.sh
@@ -1,8 +1,8 @@
 # have php-fpm run as vagrant user to enable access to the website files in the vagrant shared folder.
-sed -ir -e 's/^user *=.*/user = vagrant/' /usr/local/zend/etc/php-fpm.conf
-sed -ir -e 's/^user *=.*/user = vagrant/' /usr/local/zend/etc/php-fpm.confr
-sed -ir -e 's/^WEB_USER=.*/WEB_USER=vagrant/' /etc/zce.rc
-sed -ir -e 's/^zend.httpd_uid=.*/zend.httpd_uid=900/' /usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini
+sed -r -e 's/^user *=.*/user = vagrant/' /usr/local/zend/etc/php-fpm.conf
+sed -r -e 's/^user *=.*/user = vagrant/' /usr/local/zend/etc/php-fpm.confr
+sed -r -e 's/^WEB_USER=.*/WEB_USER=vagrant/' /etc/zce.rc
+sed -r -e 's/^zend.httpd_uid=.*/zend.httpd_uid=900/' /usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini
 
 # Set ZendServer permissions to allow php-fpm to run as vagrant user.
 chmod -R o+rw /usr/local/zend/tmp
