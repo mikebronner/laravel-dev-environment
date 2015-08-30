d287f6a2e2ab10f8d12816ea2dca77f7e630e2f2
Mike Bronner <mike@genealabs.com>
2015-08-30 09:25:00 -0700

    Fix sed commands

--- ZendServer/Homestead/post-install-permissions.sh
+++ ZendServer/Homestead/post-install-permissions.sh
@@ -1,12 +1,13 @@
 # have php-fpm run as vagrant user to enable access to the website files in the vagrant shared folder.
-sed -r -e 's/^user *=.*/user = vagrant/' /usr/local/zend/etc/php-fpm.conf
-sed -r -e 's/^WEB_USER=.*/WEB_USER=vagrant/' /etc/zce.rc
-sed -r -e 's/^zend.httpd_uid=.*/zend.httpd_uid=900/' /usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini
+sed -i -e 's/^user *=.*/user = vagrant/' /usr/local/zend/etc/php-fpm.conf
+sed -i -e 's/^WEB_USER=.*/WEB_USER=vagrant/' /etc/zce.rc
+sed -i -e 's/^zend.httpd_uid=.*/zend.httpd_uid=900/' /usr/local/zend/etc/conf.d/ZendGlobalDirectives.ini
 
 # Set ZendServer permissions to allow php-fpm to run as vagrant user.
-chmod -R o+rw /usr/local/zend/tmp
-chmod -R o+rw /usr/local/zend/var/log
-chmod -R o+rw /usr/local/zend/var/codetracing
+
+#chmod -R o+rw /usr/local/zend/tmp
+#chmod -R o+rw /usr/local/zend/var/log
+#chmod -R o+rw /usr/local/zend/var/codetracing
 
 # Restart ZendServer to apply changes.
 service zend-server restart
