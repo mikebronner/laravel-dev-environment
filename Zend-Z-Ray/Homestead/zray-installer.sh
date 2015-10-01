5f32f0f1b988c2596e01ab67abb7207707401865
Mike Bronner <mike@genealabs.com>
2015-10-01 12:21:27 -0700

    Update zray-installer.sh

--- Zend-Z-Ray/Homestead/zray-installer.sh
+++ Zend-Z-Ray/Homestead/zray-installer.sh
@@ -5,8 +5,8 @@ wget http://downloads.zend.com/zray/1208/zray-php5.6-Ubuntu-14.04-x86_64.tar.gz 
 wget https://raw.githubusercontent.com/GeneaLabs/laravel-dev-environment/master/Zend-Z-Ray/Homestead/zray.conf -O /etc/nginx/conf.d/zray.conf
 
 # Create symlinks to load Z-Ray configurations in php-fpm and php-cli:
-ln -sf /opt/zray/zray.ini /etc/php/7.0/cli/conf.d/zray.ini 
-ln -sf /opt/zray/zray.ini /etc/php5/7.0/fpm/conf.d/zray.ini
+ln -sf /opt/zray/zray.ini /etc/php/7.0/cli/conf.d/zray.conf
+ln -sf /opt/zray/zray.ini /etc/php/7.0/fpm/conf.d/zray.conf
 
 # Set file and folder permissions:
 chown -R vagrant:vagrant /opt/zray
