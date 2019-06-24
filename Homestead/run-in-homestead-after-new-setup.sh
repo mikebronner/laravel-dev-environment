86af0f42a9241b4d39d9a0556abf7ce570089513
Mike Bronner <mike@genealabs.com>
2019-06-24 12:55:17 -0700

    Update run-in-homestead-after-new-setup.sh

--- Homestead/run-in-homestead-after-new-setup.sh
+++ Homestead/run-in-homestead-after-new-setup.sh
@@ -1,5 +1,8 @@
 # Configure Redis to allow connections from host machine
-sudo sed -i '/bind 127.0.0.1 ::1/a bind 192.168.10.10' /etc/redis/redis.conf
+sudo sed -i '/^bind 127.0.0.1 ::1/a bind 192.168.10.10' /etc/redis/redis.conf
 
 # Configure PostgreSql to allow connections from host machine
 sudo bash -c "echo 'host    all             all             192.168.10.10/32        md5' >> /etc/postgresql/10/main/pg_hba.conf"
+
+sudo service redis-server restart
+sudo service postgres reload
